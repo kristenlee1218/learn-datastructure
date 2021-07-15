b92eebd29a905a20197495d08e2ceb91ed0f083b
kristen <1065200748@qq.com>
2021-07-15 21:51:26 +0800

    EP1

---
+++ src/com/learn/array/easy/EP1.java
@@ -0,0 +1,21 @@
+package com.learn.array.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2021/7/15
+ * @description :给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标
+ */
+public class EP1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("Error");
+    }
+}
