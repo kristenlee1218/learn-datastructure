a435eaaf6c1a787e4f858765d5be257803124d8e
kristen <1065200748@qq.com>
2021-07-15 22:01:05 +0800

    EP1_1

---
+++ src/com/learn/array/easy/EP1_1.java
@@ -0,0 +1,25 @@
+package com.learn.array.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Kristen
+ * @date : 2021/7/15
+ * @description :给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标
+ */
+public class EP1_1 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int partnerNumber = target - nums[i];
+            if (map.containsKey(partnerNumber)) {
+                return new int[]{map.get(partnerNumber), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
