9e336f7d1d8f008a439d7595ce3a06dde4f0530f
kristenlee1218 <1065200748@qq.com>
2021-09-01 12:01:58 +0800

    Update EP1365.java

--- src/com/learn/array/easy/EP1365.java
+++ src/com/learn/array/easy/EP1365.java
@@ -8,13 +8,12 @@ package com.learn.array.easy;
  * 以数组形式返回答案
  */
 public class EP1365 {
-
     public int[] smallerNumbersThanCurrent(int[] nums) {
         int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             int count = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[j] < nums[i]) {
+            for (int num : nums) {
+                if (num < nums[i]) {
                     count++;
                     result[i] = count;
                 }
