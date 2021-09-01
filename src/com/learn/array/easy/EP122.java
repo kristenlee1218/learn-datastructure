cb43a42d6ea5640ed313918cce17133fc4697b6b
kristenlee1218 <1065200748@qq.com>
2021-09-01 10:17:49 +0800

    Update EP122.java

--- src/com/learn/array/easy/EP122.java
+++ src/com/learn/array/easy/EP122.java
@@ -10,7 +10,7 @@ package com.learn.array.easy;
 public class EP122 {
     public int maxProfit(int[] prices) {
         int sum = 0;
-        for (int i = 1; i < prices.length; ++i) {
+        for (int i = 1; i < prices.length; i++) {
             sum += Math.max(0, prices[i] - prices[i - 1]);
         }
         return sum;
