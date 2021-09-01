357bbaf1a1d93c39bf072209c5f0be058d3609b4
kristenlee1218 <1065200748@qq.com>
2021-09-01 10:45:38 +0800

    Update EP485.java

--- src/com/learn/array/easy/EP485.java
+++ src/com/learn/array/easy/EP485.java
@@ -11,9 +11,8 @@ import java.util.List;
 public class EP485 {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0, count = 0;
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
+        for (int num : nums) {
+            if (num == 1) {
                 count++;
             } else {
                 max = Math.max(max, count);
