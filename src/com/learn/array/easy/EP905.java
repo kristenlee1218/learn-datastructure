c7c624c833d92dfa9b7666417635d40e72b5aa39
kristenlee1218 <1065200748@qq.com>
2021-09-01 11:16:08 +0800

    Update EP905.java

--- src/com/learn/array/easy/EP905.java
+++ src/com/learn/array/easy/EP905.java
@@ -9,14 +9,14 @@ public class EP905 {
     public int[] sortArrayByParity(int[] nums) {
         int[] result = new int[nums.length];
         int t = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {
-                result[t++] = nums[i];
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                result[t++] = num;
             }
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 1) {
-                result[t++] = nums[i];
+        for (int num : nums) {
+            if (num % 2 == 1) {
+                result[t++] = num;
             }
         }
         return result;
