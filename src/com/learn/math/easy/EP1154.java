4d467d4e10ba45020f544f27da08d1a0ee3346ed
kristen <1065200748@qq.com>
2021-10-23 21:41:30 +0800

    2021.10.23

--- src/com/learn/math/easy/EP1154.java
+++ src/com/learn/math/easy/EP1154.java
@@ -3,7 +3,8 @@ package com.learn.math.easy;
 /**
  * @author : Kristen
  * @date : 2021/8/6
- * @description :给一个按 YYYY-MM-DD 格式表示日期的字符串 date，请计算并返回该日期是当年的第几天。
+ * @description :给一个按 YYYY-MM-DD 格式表示日期的字符串 date，
+ * 请计算并返回该日期是当年的第几天。
  * 通常情况下，认为 1 月 1 日是每年的第 1 天，1 月 2 日是每年的第 2 天，依此类推。
  * 每个月的天数与现行公元纪年法（格里高利历）一致
  */
@@ -12,14 +13,15 @@ public class EP1154 {
         String[] dates = date.split("-");
         int year = Integer.parseInt(dates[0]);
         int month = Integer.parseInt(dates[1]);
-        int day = 0;
-        int[] days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int day = Integer.parseInt(dates[2]);
+        int result = 0;
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             days[1] = 29;
         }
         for (int i = 0; i < month - 1; i++) {
-            day += days[i];
+            result += days[i];
         }
-        return day + Integer.parseInt(dates[2]);
+        return result + day;
     }
 }
--- src/com/learn/math/easy/EP1518.java
+++ src/com/learn/math/easy/EP1518.java
@@ -3,16 +3,16 @@ package com.learn.math.easy;
 /**
  * @author : Kristen
  * @date : 2021/8/19
- * @description :小区便利店正在促销，用 numExchange 个空酒瓶可以兑换一瓶新酒。
- * 你购入了 numBottles 瓶酒。如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
+ * @description :小区便利店正在促销，用 k 个空酒瓶可以兑换一瓶新酒。
+ * 你购入了 n 瓶酒。如果喝掉了酒瓶中的酒，那么酒瓶就会变成空的。
  * 请你计算 最多 能喝到多少瓶酒。
  */
 
 public class EP1518 {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int bottle = numBottles, result = numBottles;
-        while (bottle >= numExchange) {
-            bottle -= numExchange;
+    public int numWaterBottles(int n, int k) {
+        int bottle = n, result = n;
+        while (bottle >= k) {
+            bottle -= k;
             result++;
             bottle++;
         }
