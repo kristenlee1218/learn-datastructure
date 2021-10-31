0a1ce2caa449341dbdf3978073e8c0c7342d1a6c
kristen <1065200748@qq.com>
2021-10-31 13:44:07 +0800

    2021.10.29

---
+++ src/com/learn/math/easy/Ep1523.java
@@ -0,0 +1,17 @@
+package com.learn.math.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2021/10/31
+ * @description :两个非负整数 low 和 high。请返回 low 和 high
+ * 之间（包括二者）奇数的数目
+ */
+public class Ep1523 {
+    public int countOdds(int low, int high) {
+        int count = (high - low) / 2;
+        if (low % 2 != 0 || high % 2 != 0) {
+            count++;
+        }
+        return count;
+    }
+}
