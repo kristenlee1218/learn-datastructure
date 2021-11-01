abde0bcc3e8c9bca8593c1bff3b5d7f465925c85
kristen <1065200748@qq.com>
2021-11-01 14:25:33 +0800

    2021.11.1

---
+++ src/com/learn/dynamic/easy/EP338.java
@@ -0,0 +1,23 @@
+package com.learn.dynamic.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2021/10/26
+ * @description :一个整数 n，对于 0 <= i <= n 中的每个 i，
+ * 计算其二进制表示中 1 的个数，返回一个长度为 n + 1 的数组 nums 作为答案
+ *
+ * 解析：
+ * i & (i - 1)可以去掉 i 最右边的一个 1（如果有）
+ * 因此 i & (i - 1) 是比 i 小的
+ * 而且 i & (i - 1) 的 1 的个数已经在前面算过了，
+ * 所以 i 的 1 的个数就是 i & (i - 1) 的 1 的个数 +1
+ */
+public class EP338 {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            result[i] = result[i & (i - 1)] + 1;
+        }
+        return result;
+    }
+}
--- src/com/learn/test/Test.java
+++ src/com/learn/test/Test.java
@@ -1,12 +1,19 @@
 package com.learn.test;
 
-import java.util.List;
-
 /**
  * @author : Kristen
  * @date : 2021/8/14
  * @description :
  */
 public class Test {
-
+    public boolean isSubSequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
 }
