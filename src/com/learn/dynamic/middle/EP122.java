ab721eaa3aee9cd54a1f2ec0c5666145dbb11691
kristen <1065200748@qq.com>
2021-11-01 10:57:10 +0800

    2021.11.1

---
+++ src/com/learn/dynamic/middle/EP122.java
@@ -0,0 +1,18 @@
+package com.learn.dynamic.middle;
+
+/**
+ * @author : Kristen
+ * @date : 2021/11/1
+ * @description :给定一个数组 prices，其中 prices[i] 是一支给定股票第 i 天的价格。
+ * 设计一个算法来计算所能获取的最大利润。可以尽可能地完成更多的交易（多次买卖一支股票）
+ * 注意：不能同时参与多笔交易（必须在再次购买前出售掉之前的股票）
+ */
+public class EP122 {
+    public int maxProfit(int[] prices) {
+        int sum = 0;
+        for (int i = 1; i < prices.length; i++) {
+            sum += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return sum;
+    }
+}
