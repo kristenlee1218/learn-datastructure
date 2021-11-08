3407426177b760b6997b2b80f02693472affa4e1
kristen <1065200748@qq.com>
2021-11-08 20:30:56 +0800

    2021.11.8

---
+++ src/com/learn/string/easy/EP557.java
@@ -0,0 +1,20 @@
+package com.learn.string.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2021/11/8
+ * @description :给定一个字符串，需要反转字符串中每个单词的字符顺序，
+ * 同时仍保留空格和单词的初始顺序
+ */
+public class EP557 {
+    public String reverseWords(String s) {
+        StringBuilder result = new StringBuilder();
+        String[] s1 = s.split(" ");
+        for (String value : s1) {
+            StringBuilder sb = new StringBuilder(value);
+            String afterReverse = sb.reverse().toString();
+            result.append(afterReverse).append(" ");
+        }
+        return result.substring(0, result.length() - 1);
+    }
+}
