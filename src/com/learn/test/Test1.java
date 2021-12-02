24d0f97cb51db1c0e3f8b18d2e3574baed5194bb
kristen <1065200748@qq.com>
2021-12-02 22:25:30 +0800

    2021.12.2

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -1,19 +1,15 @@
 package com.learn.test;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author : Kristen
  * @date : 2021/8/14
  * @description : EP26
  */
 public class Test1 {
-    public int removeDuplicates(int[] nums) {
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-        return i + 1;
-    }
+
 }
