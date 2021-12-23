57e34e111e6e1e023540e04ab8f35a93fc404c21
kristen <1065200748@qq.com>
2021-12-23 10:10:42 +0800

    2021.12.23

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -1,28 +1,10 @@
 package com.learn.test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author : Kristen
  * @date : 2021/8/14
  * @description :
  */
 public class Test1 {
-    public boolean isHappy(int n) {
-        Map<Integer, Integer> map = new HashMap<>();
-        while (n != 1) {
-            int count = 0;
-            while (n > 0) {
-                count += (n % 10) * (n % 10);
-                n /= 10;
-            }
-            if (map.containsKey(count)) {
-                return false;
-            }
-            map.put(count, 1);
-            n = count;
-        }
-        return true;
-    }
+
 }
