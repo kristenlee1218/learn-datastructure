dd6f5c6ed3735cd0ce1ff6dfe71f91b0475b88d9
kristen <1065200748@qq.com>
2022-01-01 22:56:43 +0800

    2022.1.1

---
+++ src/com/learn/math/easy/EP1863.java
@@ -0,0 +1,28 @@
+package com.learn.math.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2022/1/1
+ * @description :一个数组的异或总和定义为数组中所有元素按位 XOR 的结果；
+ * 如果数组为空，则异或总和为 0
+ */
+public class EP1863 {
+    int result = 0;
+
+    public int subsetXORSum(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        dfs(nums, 0, 0);
+        return result;
+    }
+
+    public void dfs(int[] nums, int i, int sum) {
+        if (i == nums.length) {
+            result += sum;
+            return;
+        }
+        dfs(nums, i + 1, sum ^ nums[i]);
+        dfs(nums, i + 1, sum);
+    }
+}
