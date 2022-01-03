e2fa9299244ef658958dd8f00061413aa2831b1e
kristen <1065200748@qq.com>
2022-01-03 23:35:05 +0800

    2022.1.3

--- src/com/learn/string/easy/EP830.java
+++ src/com/learn/string/easy/EP830.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 /**
  * @author : Kristen
- * @date : 2022/1/4
- * @description :在一个由小写字母构成的字符串 s 中，包含由一些连续的相同字
- * 符所构成的分组。例如，在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb",
+ * @date : 2022/1/3
+ * @description :在一个由小写字母构成的字符串 s 中，包含由一些连续的相同
+ * 字符所构成的分组。例如，在字符串 s = "abbxxxxzyy" 中，就含有 "a", "bb",
  * "xxxx","z" 和 "yy" 这样的一些分组。分组可以用区间 [start, end] 表示，
  * 其中 start 和 end 分别表示该分组的起始和终止位置的下标。上例中的 "xxxx"
  * 分组用区间表示为 [3,6]。所有包含大于或等于三个连续字符的分组为较大分组。
