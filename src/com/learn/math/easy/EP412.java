b7bc7bcb5689cd3bb339df686fd5cd93a0d337c2
kristen <1065200748@qq.com>
2022-01-09 20:01:14 +0800

    2022.1.9

--- src/com/learn/math/easy/EP412.java
+++ src/com/learn/math/easy/EP412.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class EP412 {
     public List<String> fizzBuzz(int n) {
-        List<String> list = new LinkedList<String>();
+        List<String> list = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 list.add("FizzBuzz");
