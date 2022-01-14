3e6632bb901492cda73d7d8552cd460411b98773
kristen <1065200748@qq.com>
2022-01-14 23:13:48 +0800

    2022.1.14

--- src/com/learn/string/easy/EP1332.java
+++ src/com/learn/string/easy/EP1332.java
@@ -7,12 +7,11 @@ package com.learn.string.easy;
  */
 public class EP1332 {
     public int removePalindromeSub(String s) {
-        int length = s.length() - 1;
         for (int i = 0; i < s.length() / 2; i++) {
-            if (s.charAt(i) != s.charAt(length - i)) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return 2;
             }
         }
-        return length == -1 ? 0 : 1;
+        return s.length() - 1 == -1 ? 0 : 1;
     }
 }
--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -1,23 +1,15 @@
 package com.learn.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 /**
  * @author : Kristen
  * @date : 2021/8/14
  * @description : EP26
  */
 public class Test1 {
-    public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for (int i = a.length() - 1, j = b.length() - 1;
-             i >= 0 || j >= 0; i--, j--) {
-            int sum = count;
-            sum += i >= 0 ? (a.charAt(i) - '0') : 0;
-            sum += j >= 0 ? (b.charAt(i) - '0') : 0;
-            sb.append(sum % 2);
-            count = sum / 2;
-        }
-        sb.append(count == 1 ? count : "");
-        return sb.reverse().toString();
-    }
+
 }
