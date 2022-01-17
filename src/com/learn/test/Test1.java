a7e827c36fe584cd007a42100029009bf3257e84
kristen <1065200748@qq.com>
2022-01-17 10:42:47 +0800

    2022.1.17

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,18 +6,5 @@ package com.learn.test;
  * @description : EP26
  */
 public class Test1 {
-    public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for (int i = a.length() - 1, j = b.length() - 1;
-             i >= 0 || j >= 0; i--, j--) {
-            int sum = count;
-            sum += i >= 0 ? (a.charAt(i) - '0') : 0;
-            sum += j >= 0 ? (b.charAt(i) - '0') : 0;
-            sb.append(sum % 2);
-            count = sum / 2;
-        }
-        sb.append(count == 1 ? count : "");
-        return sb.reverse().toString();
-    }
+
 }
--- src/com/learn/test/Test2.java
+++ src/com/learn/test/Test2.java
@@ -6,18 +6,5 @@ package com.learn.test;
  * @description :
  */
 public class Test2 {
-    public String add(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        int count = 0;
-        for (int i = 0, j = 0; i < a.length() - 1 || j < b.length() - 1; i++, j++) {
-            int sum = count;
-            sum += i > 0 ? (a.charAt(i) - '0') : 0;
-            sum += j > 0 ? (b.charAt(j) - '0') : 0;
-            sb.append(sum % 2);
-            count = sum / 2;
-        }
-        sb.append(count == 1 ? count : "");
-        return sb.reverse().toString();
-    }
 }
 
