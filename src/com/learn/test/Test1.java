07ff6e2ebd72be2d8d2dac771cece2be8832d0c8
kristen <1065200748@qq.com>
2022-03-01 22:39:01 +0800

    2022.3.1

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,5 +6,18 @@ package com.learn.test;
  * @description : EP26
  */
 public class Test1 {
-
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < strs[0].length(); i++) {
+            char temp = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || temp != strs[j].charAt(i)) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
 }
