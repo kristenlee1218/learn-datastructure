f31b0cfde338574a2db4b05faee1dc4f912e08b6
kristen <1065200748@qq.com>
2022-03-04 15:11:56 +0800

    2022.3.4

---
+++ src/com/learn/sort/easy/EP2094.java
@@ -0,0 +1,9 @@
+package com.learn.sort.easy;
+
+/**
+ * @author ：Kristen
+ * @date ：2022/3/4
+ * @description :
+ */
+public class EP2094 {
+}
--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,5 +6,12 @@ package com.learn.test;
  * @description : EP26
  */
 public class Test1 {
-
+    public int removePalindromeSub(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                return 2;
+            }
+        }
+        return s.length() - 1 == -1 ? 0 : 1;
+    }
 }
