f4435541c91985b77cd014de7f339c7ea9238558
kristen <1065200748@qq.com>
2022-03-11 16:29:27 +0800

    2022.3.11

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,15 +6,5 @@ package com.learn.test;
  * @description : EP26
  */
 public class Test1 {
-    public String capitalizeTitle(String title) {
-        String[] str = title.toLowerCase().split(" ");
-        for (int i = 0; i < str.length; i++) {
-            char[] c = str[i].toCharArray();
-            if (c.length > 2) {
-                c[0] = (char) (c[0] - 32);
-                str[i] = new String(c);
-            }
-        }
-        return String.join(" ", str);
-    }
+
 }
---
+++ src/com/learn/tree/easy/EP144.java
@@ -0,0 +1,46 @@
+package com.learn.tree.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ：Kristen
+ * @date ：2022/3/11
+ * @description :二叉树的根节点 root ，返回它节点值的前序遍历
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class EP144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+    public void preorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        preorder(root.left, list);
+        preorder(root.right, list);
+    }
+}
