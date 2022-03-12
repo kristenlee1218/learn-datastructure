626379e20590830548f9c6ea71ccc4affc2068e2
kristen <1065200748@qq.com>
2022-03-13 00:02:45 +0800

    2022.3.12

--- src/com/learn/tree/easy/EP144.java
+++ src/com/learn/tree/easy/EP144.java
@@ -3,6 +3,12 @@ package com.learn.tree.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author ：Kristen
+ * @date ：2022/3/11
+ * @description :一棵二叉树的根节点 root，返回其节点值的先序遍历
+ */
+
 public class EP144 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
