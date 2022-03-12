766364894ef37a7b47014334b17fbf1582036966
kristen <1065200748@qq.com>
2022-03-12 23:45:19 +0800

    2022.3.12

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,5 +6,24 @@ package com.learn.test;
  * @description : EP26
  */
 public class Test1 {
+    public String reverseVowels(String s) {
+        int i = 0, j = s.length() - 1;
+        char[] ch = s.toCharArray();
+        while (i < j) {
+            while (i < j && !isVowel(ch[i])) {
+                i++;
+            }
+            while (i < j && !isVowel(ch[j])) {
+                j--;
+            }
+            char c = ch[i];
+            ch[i++] = ch[j];
+            ch[j--] = c;
+        }
+        return String.valueOf(ch);
+    }
 
+    public boolean isVowel(char ch) {
+        return "aeiouAEIOU".indexOf(ch) >= 0;
+    }
 }
