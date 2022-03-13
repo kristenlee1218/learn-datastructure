69abdafead7f8646605c676f917ee585028c142c
kristen <1065200748@qq.com>
2022-03-13 15:33:47 +0800

    2022.3.13

---
+++ src/com/learn/math/easy/EP2160.java
@@ -0,0 +1,24 @@
+package com.learn.math.easy;
+
+import java.util.Arrays;
+
+/**
+ * @author : Kristen
+ * @date : 2022/3/13
+ * @description :一个四位正整数 num。请使用 num 中的数位，
+ * 将 num 拆成两个新的整数 new1 和 new2。new1 和 new2
+ * 中可以有前导 0，且 num 中所有数位都必须使用
+ */
+public class EP2160 {
+    public int minimumSum(int num) {
+        int[] a = new int[4];
+        for (int i = 0; i < 4; i++) {
+            a[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(a);
+        int n = a[0] * 10 + a[2];
+        int m = a[1] * 10 + a[3];
+        return m + n;
+    }
+}
---
+++ src/com/learn/math/easy/EP2169.java
@@ -0,0 +1,23 @@
+package com.learn.math.easy;
+
+/**
+ * @author : Kristen
+ * @date : 2022/3/13
+ * @description :两个非负整数 num1 和 num2、每一步操作中，
+ * 如果 num1 >= num2，必须用 num1 减 num2；否则，
+ * 必须用 num2 减 num1
+ */
+public class EP2169 {
+    public int countOperations(int num1, int num2) {
+        int result = 0;
+        while (num1 > 0 && num2 > 0) {
+            if (num1 >= num2) {
+                num1 -= num2;
+            } else {
+                num2 -= num1;
+            }
+            result++;
+        }
+        return result;
+    }
+}
