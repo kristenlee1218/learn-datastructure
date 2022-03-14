316d7acb60841616e51b211f4c8fb0330b4c30ed
kristen <1065200748@qq.com>
2022-03-14 14:11:59 +0800

    2022.3.14

--- src/com/learn/string/easy/EP20.java
+++ src/com/learn/string/easy/EP20.java
@@ -16,15 +16,17 @@ public class EP20 {
         }
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            char theChar = s.charAt(i);
-            if (theChar == '(' || theChar == '{' || theChar == '[') {
-                stack.push(theChar);
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
             } else {
                 if (stack.empty()) {
                     return false;
                 }
                 char preChar = stack.peek();
-                if ((preChar == '{' && theChar == '}') || (preChar == '(' && theChar == ')') || (preChar == '[' && theChar == ']')) {
+                if ((preChar == '{' && c == '}') ||
+                        (preChar == '(' && c == ')')
+                        || (preChar == '[' && c == ']')) {
                     stack.pop();
                 } else {
                     return false;
