346a67dd2e562542c5b2b827da7eea38069ba10c
kristen <1065200748@qq.com>
2022-04-08 09:48:14 +0800

    2022.4.8

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -9,5 +9,23 @@ import java.util.List;
  * @description :
  */
 public class Test1 {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        Test1.longestCommonPrefix(strs);
+    }
 
+    public static String longestCommonPrefix(String[] str) {
+        if (str.length == 0) {
+            return "";
+        }
+        for (int i = 0; i < str[0].length(); i++) {
+            char temp = str[0].charAt(i);
+            for (int j = 1; j < str.length; j++) {
+                if (i == str[j].length() || temp != str[j].charAt(i)) {
+                    return str[0].substring(0, i);
+                }
+            }
+        }
+        return str[0];
+    }
 }
