2b039eb8f5d8856185af1851981ea4ac9b9d8880
kristen <1065200748@qq.com>
2022-04-11 14:38:32 +0800

    2022.4.11

--- src/com/learn/dynamic/easy/EP1137.java
+++ src/com/learn/dynamic/easy/EP1137.java
@@ -3,9 +3,9 @@ package com.learn.dynamic.easy;
 /**
  * @author : Kristen
  * @date : 2021/8/31
- * @description :泰波那契序列 Tn 定义如下：
- * T0 = 0, T1 = 1, T2 = 1, 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2
- * 整数 n，请返回第 n 个泰波那契数 Tn 的值
+ * @description :泰波那契序列 Tn 定义如下：T0 = 0, T1 = 1, T2 = 1,
+ * 且在 n >= 0 的条件下 Tn+3 = Tn + Tn+1 + Tn+2 整数 n，请返回第 n
+ * 个泰波那契数 Tn 的值
  */
 public class EP1137 {
     public int tribonacci(int n) {
--- src/com/learn/dynamic/easy/EP746.java
+++ src/com/learn/dynamic/easy/EP746.java
@@ -3,12 +3,12 @@ package com.learn.dynamic.easy;
 /**
  * @author : Kristen
  * @date : 2021/10/26
- * @description :数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的
- * 体力花费值cost[i]（下标从 0 开始）。每当你爬上一个阶梯你都要花费对应的
- * 体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
- * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1
- * 的元素作为初始阶梯
- *
+ * @description :数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体
+ * 力花费值cost[i]（下标从 0 开始）。每当你爬上一个阶梯你都要花费对应的体力值，
+ * 一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。请找出达到
+ * 楼层顶部的最低花费。在开始时可以选择从下标为 0 或 1 的元素作为初始阶梯
+ * <p>
+ * <p>
  * 题目解析：
  * 我觉得这个题的描述应该改改：每个阶梯都有一定数量坨屎，一次只能跨一个或者
  * 两个阶梯，走到一个阶梯就要吃光上面的屎，问怎么走才能吃最少的屎？开局你
@@ -24,29 +24,4 @@ public class EP746 {
         }
         return result[cost.length];
     }
-
-   /* TEST
-   public static void main(String[] args) {
-        int[] cost = new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
-        Test1.minCostClimbingStairs(cost);
-    }
-
-    public static int minCostClimbingStairs(int[] cost) {
-        int[] result = new int[cost.length + 1];
-        result[0] = result[1] = 0;
-        for (int i = 2; i < cost.length; i++) {
-            int index = i - 1;
-            System.out.println("i: " + i);
-            System.out.println("i-1: " + index);
-            System.out.println("result[i-1]: " + result[i - 1]);
-            System.out.println("cost[i-1]: " + cost[i - 1]);
-            System.out.println("result[i-2]: " + result[i - 2]);
-            System.out.println("cost[i-2]: " + cost[i - 2]);
-            result[i] = Math.min(result[i - 1] + cost[i - 1],
-                    result[i - 2] + cost[i - 2]);
-            System.out.println("result[i]: " + result[i]);
-            System.out.println("--------------------------");
-        }
-        return result[cost.length];
-    }*/
 }
