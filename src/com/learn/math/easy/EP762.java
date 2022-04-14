d4fcb5348c1dcf141dd0bc73ea6d3a46ae3c78d0
kristen <1065200748@qq.com>
2022-04-14 16:13:47 +0800

    2022.4.14

--- src/com/learn/math/easy/EP762.java
+++ src/com/learn/math/easy/EP762.java
@@ -3,8 +3,11 @@ package com.learn.math.easy;
 /**
  * @author : Kristen
  * @date : 2021/10/29
- * @description :给定两个整数 L 和 R，找到闭区间 [L, R] 范围内，计算置位位数
- * 为质数的整数个数（注意，计算置位代表二进制表示中1的个数
+ * @description :给定两个整数 L 和 R，找到闭区间 [L, R] 范围内，
+ * 计算置位位数为质数的整数个数，计算置位代表二进制表示中 1 的个数
+ *
+ * 本质的问题就是计算在 [L, R] 范围内的数的二进制表示中，
+ * 有几个数的 1 的个数为质数
  */
 public class EP762 {
     public int countPrimeSetBits(int l, int r) {
@@ -18,6 +21,7 @@ public class EP762 {
     }
 
     public boolean isSmallPrime(int i) {
-        return (i == 2 || i == 3 || i == 5 || i == 7 || i == 11 || i == 13 || i == 17 || i == 19);
+        return (i == 2 || i == 3 || i == 5 || i == 7
+                || i == 11 || i == 13 || i == 17 || i == 19);
     }
 }
--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -1,33 +1,21 @@
 package com.learn.test;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
-
 /**
  * @author : Kristen
  * @date : 2021/8/14
  * @description :
  */
 public class Test1 {
-    public List<Integer> selfDividingNumbers(int left, int right) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = left; i <= right; i++) {
-            int temp = i;
-            while (temp > 0) {
-                if (temp % 10 == 0 || i % (temp % 10) != 0) {
-                    break;
-                }
-                temp /= 10;
-
-            }
-            if (temp == 0) {
-                list.add(i);
+    public int countPrimeSetBits(int l, int r) {
+        int result = 0;
+        for (int i = l; i <= r; i++) {
+            if (isSmallPrime(Integer.bitCount(i))) {
+                result++;
             }
         }
-        return list;
+        return result;
+    }
+
+    public boolean isSmallPrime(int i) {
+        return (i == 2 || i == 3 || i == 5 || i == 7 || i == 11 || i == 13 || i == 17 || i == 19);
     }
 }
