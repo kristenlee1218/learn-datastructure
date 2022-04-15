d71d7f0e9f400062cc14230a0bd17f43ba6cefe1
kristen <1065200748@qq.com>
2022-04-15 12:36:29 +0800

    2022.4.15

--- src/com/learn/math/easy/EP836.java
+++ src/com/learn/math/easy/EP836.java
@@ -14,7 +14,7 @@ public class EP836 {
                 rec2[0] == rec2[2] || rec2[1] == rec2[3]) {
             return false;
         }
-        return !(rec1[2] <= rec2[0] || rec1[3] <= rec2[1] ||
-                rec1[0] >= rec2[2] || rec1[1] >= rec2[3]);
+        return !(rec1[0] >= rec2[2] || rec1[1] >= rec2[3] ||
+                rec1[2] <= rec2[0] || rec1[3] <= rec2[1]);
     }
 }
