782ebded4f138e18e0eb8ede02a04844e31461f7
kristen <1065200748@qq.com>
2022-04-15 15:00:26 +0800

    2022.4.15

--- src/com/learn/math/easy/EP762.java
+++ src/com/learn/math/easy/EP762.java
@@ -6,8 +6,7 @@ package com.learn.math.easy;
  * @description :给定两个整数 L 和 R，找到闭区间 [L, R] 范围内，
  * 计算置位位数为质数的整数个数，计算置位代表二进制表示中 1 的个数
  *
- * 本质的问题就是计算在 [L, R] 范围内的数的二进制表示中，
- * 有几个数的 1 的个数为质数
+ * 本质的问题就是计算在 [L, R] 范围内的数的二进制表示中，有几个数的 1 的个数为质数
  */
 public class EP762 {
     public int countPrimeSetBits(int l, int r) {
