cf008be44f0649ca7de6657249c6a4a50d75cb63
kristen <1065200748@qq.com>
2022-04-21 10:55:48 +0800

    2022.4.21

---
+++ src/com/learn/math/easy/EP2235.java
@@ -0,0 +1,12 @@
+package com.learn.math.easy;
+
+/**
+ * @author ：Kristen
+ * @date ：2022/4/21
+ * @description :
+ */
+public class EP2235 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -1,27 +1,11 @@
 package com.learn.test;
 
-import java.util.Arrays;
-
-/**
- * @author : Kristen
- * @date : 2021/10/26
- * @description :一个整数 n。按下述规则生成一个长度为 n + 1 的数组 nums：
- * nums[0] = 0
- * nums[1] = 1
- * 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
- * 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
- * 返回生成数组 nums 中的最大值
- */
 public class Test1 {
-    public int getMaximumGenerated(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        int[] result = new int[n + 1];
-        result[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            result[i] = result[i / 2] + i % 2 * result[i / 2 + 1];
-        }
-        return Arrays.stream(result).max().getAsInt();
+    public static void main(String[] args) {
+        int left = 10, right = 20;
+        int mid1 = (right + left) / 2;
+        int mid2 = left + (right - left) / 2;
+        System.out.println(mid1);
+        System.out.println(mid2);
     }
 }
