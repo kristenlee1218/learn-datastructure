cb2c5f64fc6e5747ac36eeb24d98ace944f2f7e7
kristen <1065200748@qq.com>
2022-04-21 16:48:15 +0800

    2022.4.21

--- src/com/learn/binarySearch/easy/EP852.java
+++ src/com/learn/binarySearch/easy/EP852.java
@@ -3,17 +3,12 @@ package com.learn.binarySearch.easy;
 /**
  * @author : Kristen
  * @date : 2021/10/27
- * @description :符合下列属性的数组 arr 称为山脉数组
- * arr.length >= 3、存在 i（0 < i < arr.length - 1）使得：
- * arr[0] < arr[1] < ... arr[i-1] < arr[i]
- * arr[i] > arr[i+1] > ... > arr[arr.length - 1]
- * 给你由整数组成的山脉数组 arr ，返回任何满足
- * arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i + 1]
- * > ... > arr[arr.length - 1] 的下标 i
+ * @description : 由整数组成的山脉数组 nums，返回任何满足 nums[0] < nums[1] < ...
+ * nums[i - 1] < nums[i] > nums[i + 1] > ... > nums[nums.length - 1] 的下标 i
  */
 public class EP852 {
     public int peakIndexInMountainArray(int[] nums) {
-        for (int i = 1; i < nums.length - 1; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i] > nums[i + 1]) {
                 return i;
             }
