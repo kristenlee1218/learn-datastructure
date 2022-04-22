e881a853d3d0a3db145435cdb3a0655ee79c9998
kristen <1065200748@qq.com>
2022-04-22 13:41:32 +0800

    2022.4.22

--- src/com/learn/array/easy/EP2476.java
+++ src/com/learn/array/easy/EP2476.java
@@ -3,9 +3,8 @@ package com.learn.array.easy;
 /**
  * @author ：Kristen
  * @date ：2022/4/22
- * @description : 一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k，
- * 请返回满足 0 <= i < j < n，nums[i] == nums[j] 且 (i * j) 能被 k 整
- * 除的数对 (i, j) 的数目
+ * @description : 一个下标从 0 开始长度为 n 的整数数组 nums 和一个整数 k，请返回满足
+ * 0 <= i < j < n，nums[i] == nums[j] 且 (i * j) 能被 k 整除的数对 (i, j) 的数目
  */
 public class EP2476 {
     public int countPairs(int[] nums, int k) {
