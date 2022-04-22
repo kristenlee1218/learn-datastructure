5bb6b59717503a077870113def1fb1c0fc7277c4
kristen <1065200748@qq.com>
2022-04-22 11:04:07 +0800

    2022.4.22

--- src/com/learn/bit/easy/EP191.java
+++ src/com/learn/bit/easy/EP191.java
@@ -3,8 +3,8 @@ package com.learn.bit.easy;
 /**
  * @author ：Kristen
  * @date ：2022/3/21
- * @description :输入是一个无符号整数（以二进制串的形式），返回其二进制
- * 表达式中数字位数为 '1' 的个数（也被称为汉明重量）
+ * @description :输入是一个无符号整数（以二进制串的形式），返
+ * 回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）
  */
 public class EP191 {
     public int hammingWeight(int n) {
