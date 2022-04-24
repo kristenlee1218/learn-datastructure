6fa02ada9e39893a519bfed56164bf4f27b85ed2
kristen <1065200748@qq.com>
2022-04-24 17:59:37 +0800

    2022.4.24

--- src/com/learn/basic/search/BinarySearch_1.java
+++ src/com/learn/basic/search/BinarySearch_1.java
@@ -3,9 +3,9 @@ package com.learn.basic.search;
 /**
  * @author ：Kristen
  * @date ：2022/4/24
- * @description :二分法查
+ * @description : 二分法查
  * 二分法查（Binary Search）找适用于大的数据，但前提条件是数据必须是有序的，
- * 他的原理是先和中间的比较，如果等于就直接返回，如果小于就在前半部分继续使用
+ * 原理是先和中间的比较，如果等于就直接返回，如果小于就在前半部分继续使用
  * 二分法进行查找，如果大于则在后半部分继续使用二分法进行查找
  */
 public class BinarySearch_1 {
