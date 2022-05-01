11623ba2fe4e26dfe53b660dee25157e025d95de
kristen <1065200748@qq.com>
2022-05-01 15:26:16 +0800

    2022.5.1

--- src/com/learn/greedy/easy/EP1196.java
+++ src/com/learn/greedy/easy/EP1196.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 /**
  * @author : Kristen
  * @date : 2022/1/2
- * @description : 楼下水果店正在促销，打算买些苹果，arr[i] 表示第 i 个苹果的单位重
- * 量。有一个购物袋，最多可以装 5000 单位重量的东西，最多可以往购物袋里装入多少苹果
+ * @description : 楼下水果店正在促销，打算买些苹果，arr[i] 表示第 i 个苹果的单位
+ * 重量。有一个购物袋，最多可以装 5000 单位重量的东西，最多可以往购物袋里装入多少苹果
  */
 public class EP1196 {
     public int maxNumberOfApples(int[] nums) {
--- src/com/learn/greedy/easy/EP976.java
+++ src/com/learn/greedy/easy/EP976.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 /**
  * @author : Kristen
  * @date : 2021/8/6
- * @description : 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组成
- * 的、面积不为零的三角形的最大周长。如果不能形成任何面积不为零的三角形，返回 0
+ * @description : 给定由一些正数（代表长度）组成的数组 A，返回由其中三个长度组
+ * 成的、面积不为零的三角形的最大周长。如果不能形成任何面积不为零的三角形，返回 0
  */
 public class EP976 {
     public int largestPerimeter(int[] nums) {
