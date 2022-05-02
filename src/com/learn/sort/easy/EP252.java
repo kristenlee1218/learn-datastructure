4d85d60c42adaef90f1235f53f040d19e4d5dabb
kristen <1065200748@qq.com>
2022-05-02 14:15:58 +0800

    2022.5.1

--- src/com/learn/sort/easy/EP252.java
+++ src/com/learn/sort/easy/EP252.java
@@ -3,8 +3,8 @@ package com.learn.sort.easy;
 /**
  * @author : Kristen
  * @date : 2021/11/4
- * @description : 给定一个会议时间安排的数组 intervals，每个会议
- * 时间都会包括开始和结束的时间 intervals[i]=[starti, endi]，
+ * @description : 给定一个会议时间安排的数组 intervals，每个会
+ * 议时间都会包括开始和结束的时间 intervals[i]=[starti, endi]，
  * 请判断一个人是否能够参加这里面的全部会议
  */
 public class EP252 {
