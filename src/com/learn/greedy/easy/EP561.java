6a896341f515751989655780dc1d3e6458413b15
kristen <1065200748@qq.com>
2022-05-06 10:36:51 +0800

    2022.5.6

--- src/com/learn/greedy/easy/EP561.java
+++ src/com/learn/greedy/easy/EP561.java
@@ -5,11 +5,10 @@ import java.util.Arrays;
 /**
  * @author : Kristen
  * @date : 2021/7/15
- * @description : 给定长度为 2n 的整数数组 nums，任务是将这些
- * 数分成 n 对，如(a1, b1),(a2, b2),..., (an, bn)，使得从 1
- * 到 n 的 min(ai, bi) 总和最大
+ * @description : 给定长度为 2n 的整数数组 nums，将这些数分成 n 对，
+ * 如(a1, b1),(a2, b2),..., (an, bn)，使得从 1到 n 的 min(ai, bi)
+ * 总和最大
  */
-
 public class EP561 {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
