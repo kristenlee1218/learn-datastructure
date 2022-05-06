4a5e7d6cdb077df3a21e2428c1564e9e6e12d567
kristen <1065200748@qq.com>
2022-05-06 14:05:50 +0800

    2022.5.6

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -11,5 +11,12 @@ import java.util.List;
  * @description :
  */
 public class Test1 {
-
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
 }
