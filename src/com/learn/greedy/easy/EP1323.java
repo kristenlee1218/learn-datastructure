5e8678f967f5a99ed9ba02d0351fad58f922160e
kristen <1065200748@qq.com>
2022-05-09 20:20:17 +0800

    2022.5.9

--- src/com/learn/greedy/easy/EP1323.java
+++ src/com/learn/greedy/easy/EP1323.java
@@ -3,7 +3,7 @@ package com.learn.greedy.easy;
 /**
  * @author : Kristen
  * @date : 2021/8/6
- * @description : 一个仅由数字 6 和 9 组成的正整数 num。最多只能翻
+ * @description : 一个仅由数字 6 和 9 组成的正整数 n。最多只能翻
  * 转一位数字，将 6 变成 9，或把 9 变成 6、请返回可以得到的最大数字
  */
 public class EP1323 {
