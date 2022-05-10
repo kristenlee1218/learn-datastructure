372ac7d98f34b927327290238e0069ce9e1f4975
kristen <1065200748@qq.com>
2022-05-10 15:12:35 +0800

    2022.5.10

---
+++ src/com/learn/hashtable/easy/EP705.java
@@ -0,0 +1,71 @@
+package com.learn.hashtable.easy;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ * @author ：Kristen
+ * @date ：2022/5/10
+ * @description :
+ */
+
+class MyHashSet {
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; ++i) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                data[h].remove(element);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Returns true if this set contains the specified element
+     */
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            if (element == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
+
+public class EP705 {
+}
