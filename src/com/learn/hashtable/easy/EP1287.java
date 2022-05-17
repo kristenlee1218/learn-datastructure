6b1a41cb796d5a620964f698a6bd05f175ddf268
kristen <1065200748@qq.com>
2022-05-17 23:45:11 +0800

    2022.5.17

--- src/com/learn/hashtable/easy/EP1287.java
+++ src/com/learn/hashtable/easy/EP1287.java
@@ -6,7 +6,8 @@ import java.util.Map;
 /**
  * @author : Kristen
  * @date : 2022/5/17
- * @description :
+ * @description : 一个非递减的有序整数数组，已知这个数组中恰好有
+ * 一个整数，它的出现次数超过数组元素总数的 25%、请返回这个整数
  */
 public class EP1287 {
     public int findSpecialInteger(int[] nums) {
