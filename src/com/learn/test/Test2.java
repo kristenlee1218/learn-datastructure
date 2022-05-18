bcb0d1db756043f53c597a18dd5178852198c77d
kristen <1065200748@qq.com>
2022-05-18 23:14:00 +0800

    2022.5.18

--- src/com/learn/test/Test2.java
+++ src/com/learn/test/Test2.java
@@ -6,5 +6,12 @@ package com.learn.test;
  * @description :
  */
 public class Test2 {
+    public static void main(String[] args) {
+        String a = new String("abc").intern();
+        String b = new String("abc").intern();
+        if (a == b) {
+            System.out.print("a==b");
+        }
+    }
 }
 
