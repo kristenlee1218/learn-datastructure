477873a61de6365936d4e0be752392dc56ffaaab
kristen <1065200748@qq.com>
2022-05-24 17:52:16 +0800

    2022.5.24

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -8,7 +8,17 @@ import java.util.*;
  * @description :
  */
 public class Test1 {
+    public boolean hasGroupsSizeX(int[] nums) {
+        int[] result = new int[10000];
+        for (int num : nums) {
+            result[num]++;
+        }
+        return Arrays.stream(result).reduce(this::gcd).getAsInt() > 1;
+    }
 
+    private int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 }
 
 
