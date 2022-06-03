4b9d2c81f3c4263e900a349f63f1038a1c2fb7de
kristen <1065200748@qq.com>
2022-06-03 22:26:26 +0800

    2022.6.3

--- src/com/learn/hashtable/easy/EP1460.java
+++ src/com/learn/hashtable/easy/EP1460.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 /**
  * @author : Kristen
  * @date : 2021/8/14
- * @description : 两个长度相同的整数数组 target 和 arr、每一步中可以选择 arr 的任意
- * 非空子数组并将它翻转。可以执行此过程任意次。如果能让 arr 变得与 target 相同，返回
- * true；否则返回 false
+ * @description : 两个长度相同的整数数组 target 和 arr、每一步中可以选择 arr
+ * 的任意非空子数组并将它翻转。可以执行此过程任意次。如果能让 arr 变得与 target
+ * 相同，返回 true；否则返回 false
  */
 public class EP1460 {
     public boolean canBeEqual(int[] target, int[] arr) {
