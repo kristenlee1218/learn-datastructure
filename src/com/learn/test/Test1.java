0b1c7a663137914ec9392d4e6f0543177ca067a8
kristen <1065200748@qq.com>
2022-06-09 10:08:05 +0800

    2022.6.9

--- src/com/learn/test/Test1.java
+++ src/com/learn/test/Test1.java
@@ -6,12 +6,17 @@ package com.learn.test;
  * @description :
  */
 public class Test1 {
-    public int singleNumber(int[] nums) {
-        int single = 0;
-        for (int num : nums) {
-            single ^= num;
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i > index) {
+                    nums[index] = nums[i];
+                    nums[i] = 0;
+                }
+                index++;
+            }
         }
-        return single;
     }
 }
 
