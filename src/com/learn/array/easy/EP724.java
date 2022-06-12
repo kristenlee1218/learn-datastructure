094727599513e2c5c845a7794ac8de3dd8e314d2
kristen <1065200748@qq.com>
2022-06-12 22:48:45 +0800

    2022.6.12

--- src/com/learn/array/easy/EP724.java
+++ src/com/learn/array/easy/EP724.java
@@ -4,15 +4,14 @@ package com.learn.array.easy;
  * @author : Kristen
  * @date : 2021/8/6
  * @description : 给定一个整数类型的数组 nums，请编写一个能够返回数组
- * “中心索引” 的方法。如果数组不存在中心索引，返回 -1。如果数组有多个
- * 中心索引，应该返回最靠近左边的那一个
+ * “中心索引” 的方法。如果数组不存在中心索引，返回 -1。如果数组有多个中
+ * 心索引，应该返回最靠近左边的那一个
  *
- * 中心索引的：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和
+ * 中心索引：数组中心索引的左侧所有元素相加的和等于右侧所有元素相加的和
  */
 public class EP724 {
     public int pivotIndex(int[] nums) {
-        int sum = 0;
-        int leftSum = 0;
+        int sum = 0, leftSum = 0;
         for (int num : nums) {
             sum += num;
         }
