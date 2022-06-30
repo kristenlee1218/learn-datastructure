092945de3b8d2003fb6b1fa12329b24fc6f8c379
kristen <1065200748@qq.com>
2022-06-30 10:22:00 +0800

    2022.6.30

--- src/com/learn/string/easy/EP482.java
+++ src/com/learn/string/easy/EP482.java
@@ -3,7 +3,10 @@ package com.learn.string.easy;
 /**
  * @author ：Kristen
  * @date ：2022/6/20
- * @description :
+ * @description : 给定一个许可密钥字符串 s，仅由字母、数字字符和破折号组成。字符串
+ * 由 n 个破折号分成 n + 1 组、也会得到一个整数 k。想要重新格式化字符串 s，使每一组
+ * 包含 k 个字符，除了第一组，它可以比 k 短，但仍然必须包含至少一个字符。此外两组之间
+ * 必须插入破折号，并且应该将所有小写字母转换为大写字母。返回重新格式化的许可密钥
  */
 public class EP482 {
     public String licenseKeyFormatting(String s, int k) {
