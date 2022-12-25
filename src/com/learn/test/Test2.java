020a87e1b5184dc9def588a880a9893b4f09f12c
kristen <1065200748@qq.com>
2022-12-25 17:10:57 +0800

    2022.12.25

--- src/com/learn/test/Test2.java
+++ src/com/learn/test/Test2.java
@@ -1,11 +1,16 @@
 package com.learn.test;
 
+import java.util.UUID;
+
 /**
  * @author : Kristen
  * @date : 2021/11/5
  * @description :
  */
 public class Test2 {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            System.out.println(UUID.randomUUID());
+        }
+    }
 }
-
