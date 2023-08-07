5b74442aea88a995212496a8ac2550416205374c
kristen <1065200748@qq.com>
2023-08-07 16:29:51 +0800

    2023.8.7

--- src/com/learn/array/easy/EP2496.java
+++ src/com/learn/array/easy/EP2496.java
@@ -5,7 +5,7 @@ package com.learn.array.easy;
  * @date ：2023/4/21
  * @description : 一个由字母和数字组成的字符串的值定义如下：
  * 如果字符串只包含数字，那么值为该字符串在 10 进制下的所表示的数字。否则，值为字符串的长度。
- * 给你一个字符串数组 strs，每个字符串都只由字母和数字组成，请你返回 strs中字符串的最大值。
+ * 一个字符串数组 strs，每个字符串都只由字母和数字组成，请返回 strs中字符串的最大值。
  */
 public class EP2496 {
     public int maximumValue(String[] strs) {
---
+++ src/com/learn/test/1.sql
@@ -0,0 +1,4 @@
+create table1 (
+    id number,
+    name varchar(100)
+);
