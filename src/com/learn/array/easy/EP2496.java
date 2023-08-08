faf29ce24058bae57d164f194230cacd6f637f3d
kristen <1065200748@qq.com>
2023-08-08 10:58:46 +0800

    2023.8.7

--- src/com/learn/array/easy/EP2496.java
+++ src/com/learn/array/easy/EP2496.java
@@ -5,21 +5,21 @@ package com.learn.array.easy;
  * @date ：2023/4/21
  * @description : 一个由字母和数字组成的字符串的值定义如下：
  * 如果字符串只包含数字，那么值为该字符串在 10 进制下的所表示的数字。否则，值为字符串的长度。
- * 一个字符串数组 strs，每个字符串都只由字母和数字组成，请返回 strs中字符串的最大值。
+ * 一个字符串数组 str，每个字符串都只由字母和数字组成，请返回 str 中字符串的最大值。
  */
 public class EP2496 {
-    public int maximumValue(String[] strs) {
+    public int maximumValue(String[] str) {
         int count = 0;
-        for (String str : strs) {
+        for (String s : str) {
             boolean isDigit = true;
-            for (char c : str.toCharArray()) {
+            for (char c : s.toCharArray()) {
                 if (Character.isLetter(c)) {
                     isDigit = false;
-                    count = Math.max(count, str.length());
+                    count = Math.max(count, s.length());
                 }
             }
             if (isDigit) {
-                count = Math.max(count, Integer.parseInt(str));
+                count = Math.max(count, Integer.parseInt(s));
             }
         }
         return count;
