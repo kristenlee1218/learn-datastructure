e75156703997451cc56f56ceccdaf5a76a3c978b
kristen <1065200748@qq.com>
2023-08-09 17:17:04 +0800

    2023.8.9

--- src/com/learn/math/easy/EP2520.java
+++ src/com/learn/math/easy/EP2520.java
@@ -6,13 +6,13 @@ package com.learn.math.easy;
  * @description :
  */
 public class EP2520 {
-    public int countDigits(int num) {
+    public int countDigits(int n) {
         int count = 0;
-        int tmp = num;
-        while (tmp > 0) {
-            int cur = tmp % 10;
-            count += (num % cur) == 0 ? 1 : 0;
-            tmp /= 10;
+        int temp = n;
+        while (temp > 0) {
+            int cur = temp % 10;
+            count += (n % cur) == 0 ? 1 : 0;
+            temp /= 10;
         }
         return count;
     }
