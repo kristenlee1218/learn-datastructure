6f918979bb33e1ccd79fb8f8d5dd3fb549e66daf
kristen <1065200748@qq.com>
2023-08-10 10:52:03 +0800

    2023.8.10

--- src/com/learn/math/easy/EP2520.java
+++ src/com/learn/math/easy/EP2520.java
@@ -3,7 +3,8 @@ package com.learn.math.easy;
 /**
  * @author ：Kristen
  * @date ：2023/8/9
- * @description :
+ * @description : 一个整数 n，返回 n 中能整除 n 的数位的数目。如果满足 nums % val == 0，
+ * 则认为整数 val 可以整除 n
  */
 public class EP2520 {
     public int countDigits(int n) {
