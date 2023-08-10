b532f9912e96a75fcad1680b372099f7fa52e105
kristen <1065200748@qq.com>
2023-08-10 16:03:45 +0800

    2023.8.10

--- src/com/learn/math/easy/EP2651.java
+++ src/com/learn/math/easy/EP2651.java
@@ -3,7 +3,7 @@ package com.learn.math.easy;
 /**
  * @author ：Kristen
  * @date ：2023/8/9
- * @description : 一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给你一个正
+ * @description : 一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给一个正
  * 整数 delayedTime 表示列车延误的小时数。返回列车实际到站的时间
  */
 public class EP2651 {
