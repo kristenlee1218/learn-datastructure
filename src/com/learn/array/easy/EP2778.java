8c8b1996c3e9972811004df34ebeec6db4b9ee4f
kristen <1065200748@qq.com>
2023-08-11 16:52:58 +0800

    2023.8.11

--- src/com/learn/array/easy/EP2778.java
+++ src/com/learn/array/easy/EP2778.java
@@ -3,17 +3,14 @@ package com.learn.array.easy;
 /**
  * @author ：Kristen
  * @date ：2023/8/11
- * @description :
+ * @description : 一个下标从 1 开始、长度为 n 的整数数组 nums。对 nums 中的元素 nums[i]如果 n 能
+ * 够被 i 整除，即 n % i == 0，则认为 num[i] 是一个 特殊元素。返回 nums 中所有特殊元素的平方和
  */
 public class EP2778 {
     public int sumOfSquares(int[] nums) {
-        // 记录结果
         int count = 0, len = nums.length;
-        // 在内存中的下标依旧为0
         for (int i = 0; i < len; i++) {
-            // 题干中下标从1开始
             if (len % (i + 1) == 0) {
-                // 记录符合条件的平方和
                 count += nums[i] * nums[i];
             }
         }
--- src/com/learn/array/easy/EP2784.java
+++ src/com/learn/array/easy/EP2784.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 /**
  * @author ：Kristen
  * @date ：2023/8/11
- * @description :
+ * @description : 一个整数数组 nums，如果它是数组 base[n] 的一个排列，称它是个好数组。
+ * base[n] = [1, 2, ..., n - 1, n, n] （换句话说，它是一个长度为 n + 1 且包含 1 到
+ * n - 1 恰好各一次，包含 n 两次的一个数组）。比方说，base[1] = [1, 1]，base[3]
+ * = [1, 2, 3, 3]。如果数组是一个好数组，请返回 true，否则返回 false
  */
 public class EP2784 {
     public boolean isGood(int[] nums) {
