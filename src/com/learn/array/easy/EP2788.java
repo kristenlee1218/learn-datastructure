a4cfb9e1a7c09b9dd13c83d2330af4465061cee6
kristen <1065200748@qq.com>
2023-08-11 13:44:23 +0800

    2023.8.11

---
+++ src/com/learn/array/easy/EP2788.java
@@ -0,0 +1,27 @@
+package com.learn.array.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author ：Kristen
+ * @date ：2023/8/11
+ * @description : 一个字符串数组 words 和一个字符 separator，请按 separator 拆分 words 中的每个字符串。
+ * 返回一个由拆分后的新字符串组成的字符串数组，不包括空字符串
+ */
+public class EP2788 {
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> list = new ArrayList<>();
+        String s = String.valueOf("\\" + separator);
+        for (String word : words) {
+            String[] splitWords = word.split(s);
+            for (String splitWord : splitWords) {
+                if (!splitWord.isEmpty()) {
+                    list.add(splitWord);
+                }
+            }
+        }
+        return list;
+    }
+}
